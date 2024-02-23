96fb8a5fd34a35c879e1f562013748f4c8d6d207
sinancemvural <sinancemvural@gmail.com>
2024-02-23 12:33:03 +0300

    MVC design pattern review

---
+++ NetCore8MVC/Controllers/CarController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore8MVC.Controllers
+{
+    public class CarController : Controller
+    {
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCore8MVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using NetCore8MVC.Models;
+using System.Diagnostics;
+
+namespace NetCore8MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+        public IActionResult Iletisim()
+        {
+            return View();
+        }      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
