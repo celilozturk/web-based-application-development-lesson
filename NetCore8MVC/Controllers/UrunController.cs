c6d9932475d48d4da906485f695098e123a50d07
sinancemvural <sinancemvural@gmail.com>
2024-02-23 13:44:56 +0300

    Model bilgisi ve controllerdan viewe Model transfer

---
+++ NetCore8MVC/Controllers/UrunController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore8MVC.Models;
+
+
+
+namespace NetCore8MVC.Controllers
+{
+    public class UrunController : Controller
+    {
+        //Constructor kısayol => ctor
+        public UrunController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            //Urun nesnesi !
+            Urun urun1 = new Urun(); // Nesne yaratma
+            urun1.UrunAdi = "Lenovo Thinkpad E14gen5";
+            urun1.Fiyat = 7999;
+            urun1.UrunStokAdedi = 80;
+                       
+
+            return View(urun1);
+        }
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+    }
+}
---
+++ NetCore8MVC/Models/Urun.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCore8MVC.Models
+{
+    public class Urun
+    {
+        //Property kısayol => prop + tab + tab
+        public string UrunAdi { get; set; }
+        public int UrunStokAdedi { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+}
