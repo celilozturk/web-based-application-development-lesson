1c86d9a441f46c489d4426176fe7e21225e3b816
Celil Ozturk <celil.ozturk@windowslive.com>
2024-02-25 13:26:02 +0300

    Labs 1

---
+++ NetCore8MVC/Controllers/ArabaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore8MVC.Controllers
+{
+    public class ArabaController : Controller
+    {
+
+        public ArabaController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            // Araba nesnesini buraya yaratınız !
+
+
+
+
+            return View();
+        }
+
+        // Araştırma: Form ile kullanıcıdan alınacak olan verilerin Ekle Metodu altında karşılanması sağlanacaktır!
+         public IActionResult Ekle()
+        {
+            //Kullanıcının gönderdiği veriler burada karşılanacaktır.
+
+
+
+            return View();
+        }
+    }
+}
--- NetCore8MVC/Controllers/UrunController.cs
+++ NetCore8MVC/Controllers/UrunController.cs
@@ -10,20 +10,21 @@ namespace NetCore8MVC.Controllers
         //Constructor kısayol => ctor
         public UrunController()
         {
-            
+
         }
 
         public IActionResult Index()
         {
             //Urun nesnesi !
             Urun urun1 = new Urun(); // Nesne yaratma
-            urun1.UrunAdi = "Lenovo Thinkpad E14gen5";
+            urun1.UrunAdi = "Lenovo Thinkpad E14gen5"; // Oluşturulan nesnenin özelliklerine değer ataması yapılması.
             urun1.Fiyat = 7999;
             urun1.UrunStokAdedi = 80;
-                       
 
-            return View(urun1);
+
+            return View(urun1);  //  urun1 adındaki nesnenin UrunController'dan View'e aktarılması.
         }
+        
         public IActionResult Ekle()
         {
             return View();
